a7558fd5194620826e81bef345a8361490c66c4f
Priyank <Priyankk2405@gmail.com>
2026-01-21 22:56:40 +0530

    modify the nav bar

--- src/components/Hearder.jsx
+++ src/components/Hearder.jsx
@@ -1,5 +1,15 @@
-import { PiCaretDownLight } from "react-icons/pi";
 import { useState } from "react";
+import { PiCaretDownLight } from "react-icons/pi";
+import { IoIosSearch } from "react-icons/io";
+import { CiDiscount1 } from "react-icons/ci";
+import { IoIosHelpBuoy } from "react-icons/io";
+import { MdOutlinePersonOutline } from "react-icons/md";
+import { BsCart } from "react-icons/bs";
+
+
+
+
+
 
 export const Header = () => {
     const [toggle, setToggle] = useState(false)
@@ -10,6 +20,28 @@ export const Header = () => {
     const hideSideMenu =() =>{
         setToggle(false);
     }
+    const links = [
+      {
+        icon:<IoIosSearch />,
+        name:"Search"
+      },
+      {
+        icon:<CiDiscount1 />,
+        name:"Discount"
+      },
+      {
+        icon:<IoIosHelpBuoy />,
+        name:"Help"
+      },
+      {
+        icon:<MdOutlinePersonOutline />,
+        name:"Sign In"
+      },
+      {
+        icon:<BsCart />,
+        name:"Cart"
+      }
+    ]
 
   return (
     <>
@@ -22,20 +54,21 @@ export const Header = () => {
         }}></div>
     </div>
       <header className="p-[15px] shadow-xl text-[#686b78]">
-        <div className="max-w-[1200px] mx-auto border border-red-500 flex items-center">
+        <div className="max-w-[1200px] mx-auto flex items-center">
           <div className="w-[100px]">
             <img src="images/logo.png" alt="swiggy logo" className="w-full" />
           </div>
           <div> 
             <span className="font-bold border-b-[3px]">Rispana Pull</span> Dehradun, Uttarakhand, India  <PiCaretDownLight  fontSize={20} className="inline text-[#fc8019] cursor-pointer " onClick={showSideMenu}/> </div>
-            <nav className="ml-auto">
-                <ul className="flex gap-5 font-semibold text-[18px]">
-                    <li>Search</li>
-                    <li>Offers</li>
-                    <li>Help</li>
-                    <li>Sign In</li>
-                    <li>Cart</li>
-                </ul>
+            <nav className="ml-auto flex list-none gap-10 font-semibold text-[18px]">
+              {
+                links.map((link, index) =>{
+                 return <li  key={index} className="flex hover:text-[#fc8019] items-center gap-2">
+                      {link.icon}
+                      {link.name}
+                    </li>
+                })
+              }
             </nav>
         </div>
       </header>
